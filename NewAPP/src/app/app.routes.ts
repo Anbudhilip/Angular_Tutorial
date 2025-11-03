9e6b426ffc10587eb1a017a8ea36f0be67187a3e
CIPL1759 <CIPL1759@CIPL-ATC00591>
2025-11-03 19:22:41 +0530

    update items

--- NewAPP/src/app/app.routes.ts
+++ NewAPP/src/app/app.routes.ts
@@ -1,4 +1,5 @@
-import { Routes } from '@angular/router';
+
+import { RouterModule, Routes } from '@angular/router';
 import { HomePage } from './home-page/home-page';
 import { AboutPage } from './about-page/about-page';
 import {App} from '../app/app';
@@ -7,6 +8,9 @@ import { SideBar } from './home-page/side-bar/side-bar';
 import { ContentPage } from './home-page/content-page/content-page';
 import { CURD } from './curd/curd';
 import { CURDHTTP } from './curdhttp/curdhttp';
+import { Edit } from './curdhttp/edit/edit';
+import { Create } from './curdhttp/create/create';
+
 
 export const routes: Routes = [
 
@@ -14,7 +18,14 @@ export const routes: Routes = [
     {path:'about',component:CURDHTTP},
     {path:'content',component:ContentPage},
     {path:'curd',component:CURD},
-    {path:'curds',component:CURDHTTP}
+    {path:'curds',component:CURDHTTP},
+    {path:'curds/create',component:Create },
+    {path:'curds/edit/:TopicId',component:Edit}  
+      // children :
+      // [
+      //   {path:'create',component:Create },
+      //   {path:'edit/:TopicId',component:Edit}   // children route create httpcurd view and insert the routelet
+      // ]}
     // {path:'**',redirectTo:''},
   //  { path: '', redirectTo: 'home', pathMatch: 'full' },
     //{ path: '', redirectTo: '/dashboard', pathMatch: 'full' },
